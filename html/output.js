6e5bfe6edbfd64dacf6ea21a051226a41e6e369d
iphren <contact@iphren.com>
2020-04-17 15:13:11 +0100

    minor update

---
+++ html/output.js
@@ -0,0 +1,59 @@
+const remote = require('electron').remote;
+const fs = require('fs');
+const app = remote.app;
+const input = document.getElementById('input');
+const path = document.getElementById('path');
+const comment = 'comment';
+
+fs.writeFileSync(app.line, '');
+path.value = app.line;
+path.addEventListener('focus', function() {
+    path.setSelectionRange(0, path.value.length);
+});
+
+try {
+    var lyrics = fs.readFileSync(app.lyrics, { encoding: 'utf-8' });
+} catch (e) {
+    var lyrics = '';
+};
+
+input.value = lyrics;
+update();
+input.addEventListener('input', updated);
+
+function updated(e) {
+    fs.writeFileSync(app.lyrics, input.value);
+    update();
+};
+
+function update() {
+    let output = document.getElementById('output');
+    if (output) output.remove();
+    let lines = input.value
+        .replace(/^[\s\n]+|[\s\n]+$/g,'')
+        .replace(/^\n+/gm,'\n')
+        .split('\n');
+    let html = '<select id="output" onchange="change()" multiple>';
+    html += `<option class="${comment}" value="-1"></option>`;
+    for (let l = 0; l < lines.length; l++) {
+        html += '<option ';
+        if (lines[l][0] == '#') html += `class="${comment}" `;
+        html += 'value="' + l + '">' + lines[l].replace(/^#/,'');
+        html += '</option>';
+    };
+    html += `<option class="${comment}" value="`;
+    html += lines.length.toString() + '">. . .</option>';
+    html += '</select>';
+    document.getElementById('right').innerHTML = html;
+};
+
+function change() {
+    let opts = document.getElementById('output').selectedOptions;
+    let text = '';
+    for (let o of opts) {
+        if (o.className == comment) continue;
+        if (/^\s*$/.test(o.innerHTML)) continue; 
+        text += o.innerHTML + '\n';
+    };
+    fs.writeFileSync(app.line, text);
+};
--- index.js
+++ index.js
@@ -1,28 +1,66 @@
 const { app, BrowserWindow } = require('electron')
+const { ipcMain } = require('electron')
 const path = require('path')
 const fs = require('fs')
 
 const appData = path.join(app.getPath('appData'), 'obs-lyrics')
 app.lyrics = path.join(appData, 'lyrics.txt')
 app.line = path.join(appData, 'line.txt')
+app.settings = path.join(appData, 'settings.json')
 fs.mkdirSync(appData, { recursive: true })
+
+try {
+  app.set = JSON.parse(fs.readFileSync(app.settings))
+  if (app.set.maximize) throw 'maximized'
+} catch (e) {
+  app.set = {rec: {width: 800, height: 600}, showleft: true}
+}
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: app.set.rec.width,
+    height: app.set.rec.height,
     webPreferences: {
       nodeIntegration: true
     }
   })
+  try {win.setBounds(app.set.rec)} catch (e) {}
+  app.set.rec = win.getBounds()
   win.loadFile('html/index.html')
   win.setMenu(null)
   //win.webContents.openDevTools()
+
+  win.on('resize', () => {
+    app.set.rec = win.getBounds()
+    save(app.set, app.settings)
+  })
+  win.on('move', () => {
+    app.set.rec = win.getBounds()
+    save(app.set, app.settings)
+  })
+  win.on('maximize', () => {
+    app.set.maximize = true
+    save(app.set, app.settings)
+  })
+  win.on('unmaximize', () => {
+    app.set.maximize = false
+    save(app.set, app.settings)
+  })
+
+  ipcMain.on('resize', (e, size) => {
+    app.set.rec.width += size
+    app.set.rec.x -= size
+    app.set.showleft = size > 0
+    try {win.setBounds(app.set.rec)} catch (e) {}
+    app.set.rec = win.getBounds()
+    save(app.set, app.settings)
+  })
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
+  fs.writeFileSync(app.line, '')
   if (process.platform !== 'darwin') {
     app.quit()
   }
@@ -33,3 +71,14 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+function save(data, file)  {
+  try {
+    var content = JSON.stringify(data)
+  } catch (e) {
+    return false
+  }
+  var stream = fs.createWriteStream(file)
+  stream.end(content)
+  return true
+}
