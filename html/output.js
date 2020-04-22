0a629affb6e8148abe6a97bb2318cea9cfc6fad6
iphren <contact@iphren.com>
2020-04-22 19:55:01 +0100

    shortcut added

--- html/output.js
+++ html/output.js
@@ -4,6 +4,10 @@ const app = remote.app;
 const input = document.getElementById('input');
 const path = document.getElementById('path');
 const comment = 'comment';
+const sequence = ['QWERTYUIO','ASDFGHJKL'];
+
+var group = {};
+var pause = [];
 
 fs.writeFileSync(app.line, '');
 path.value = app.line;
@@ -30,10 +34,10 @@ function update() {
     let output = document.getElementById('output');
     if (output) output.remove();
     let lines = input.value
-        .replace(/^[\s\n]+|[\s\n]+$/g,'')
+        .replace(/^\n+|\n+$/g,'')
         .replace(/^\n+/gm,'\n')
         .split('\n');
-    let html = '<select id="output" onchange="change()" multiple>';
+    let html = '<select id="output" onchange="change()" onkeydown="response(event)" multiple>';
     html += `<option class="${comment}" value="-1"></option>`;
     for (let l = 0; l < lines.length; l++) {
         html += '<option ';
@@ -43,17 +47,108 @@ function update() {
     };
     html += `<option class="${comment}" value="`;
     html += lines.length.toString() + '">. . .</option>';
-    html += '</select>';
+    html += '</select><div id="west"></div><div id="east"></div>';
     document.getElementById('right').innerHTML = html;
 };
 
 function change() {
-    let opts = document.getElementById('output').selectedOptions;
+    let otpt = document.getElementById('output');
+    let opts = otpt.options;
+    let indx = otpt.selectedIndex;
+    let sels = otpt.selectedOptions;
     let text = '';
-    for (let o of opts) {
+    let line = 0;
+    let last = indx;
+    for (let o of sels) {
         if (o.className == comment) continue;
         if (/^\s*$/.test(o.innerHTML)) continue; 
-        text += o.innerHTML + '\n';
+        line++;
+        text += o.innerHTML.replace(/^\s+|\s+$/,'') + '\n';
+        o.className = '';
+        last = o.index;
     };
     fs.writeFileSync(app.line, text);
+    if (line == 0) line = sels.length;
+    if (line == 0) return;
+    group = {};
+    let count = 0;
+    for (let i = indx - 1; i >= 0; i--) {
+        if (opts[i].className == comment) continue;
+        if (/^\s*$/.test(opts[i].innerHTML)) continue;
+        if (Math.floor(count/line) >= sequence[0].length) {
+            opts[i].className = '';
+            continue;
+        };
+        let key = sequence[0][Math.floor(count/line)];
+        if (!(key in group)) group[key] = [];
+        group[key].push(opts[i]);
+        if (count % line != line - 1) {
+            opts[i].className = '';
+        } else {
+            opts[i].className = key;
+        };
+        count++;
+    };
+    count = 0;
+    for (let i = last + 1; i < opts.length; i++) {
+        if (opts[i].className == comment) continue;
+        if (/^\s*$/.test(opts[i].innerHTML)) continue;
+        if (Math.floor(count/line) >= sequence[1].length) {
+            opts[i].className = '';
+            continue;
+        }
+        let key = sequence[1][Math.floor(count/line)];
+        if (!(key in group)) group[key] = [];
+        group[key].push(opts[i]);
+        if (count % line != 0) {
+            opts[i].className = '';
+        } else {
+            opts[i].className = key;
+        };
+        count++;
+    };
+};
+
+function response(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    let otpt = document.getElementById('output');
+    let key = e.key;
+    switch (key) {
+        case 'ArrowUp':
+            key = 'Q';
+            break;
+        case 'ArrowDown':
+            key = 'A';
+            break;
+        case ' ':
+            if (pause.length == 0) {
+                for (let o of otpt.selectedOptions) {
+                    if (o.className == comment) continue;
+                    if (/^\s*$/.test(o.innerHTML)) continue; 
+                    pause.push(o);
+                    o.className = 'SPACE';
+                };
+                otpt.selectedIndex = -1;
+                fs.writeFileSync(app.line, '');
+            } else {
+                pause.forEach(o => {o.className = '';o.selected = true});
+                pause = [];
+                change();
+            };
+            return;
+        default:
+            key = key.toUpperCase();
+    };
+    if (key in group) {
+        pause.forEach(o => o.className = '');
+        pause = [];
+        let top = sequence[0].indexOf(key) > -1;
+        otpt.selectedIndex = -1;
+        for (let o of group[key]) {
+            o.selected = true;
+            o.scrollIntoView(top);
+        };
+        change();
+    }
 };
